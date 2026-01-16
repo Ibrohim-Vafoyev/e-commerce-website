0d9d5a50b0df7dd75b6104c1acfa165f07a06d1d
Abdujabbor <karimberdiyevabdujabbor12@gmail.com>
2026-01-16 09:46:40 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <div className=' text-red-200'>hello</div>
+    </>
+  )
+}
+
+export default App
